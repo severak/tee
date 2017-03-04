fff5ea7748f197f5ff02757780d4fdb91800c3c9
Severak <strajt9@seznam.cz>
2017-03-04 20:36:27 +0100

    hp

--- lib/views/homepage.php
+++ lib/views/homepage.php
@@ -1,3 +1,119 @@
-<h1>Soup is down...</h1>
-<p>So I am working on <em>resoUp</em>, Soup.io self-hosted clone.</p>
-<img src='/st/404.jpg'>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Trans Europ Express">
+    <title>Trans Europ Express</title>
+    
+    <link rel="stylesheet" href="https://unpkg.com/purecss@0.6.2/build/pure-min.css" integrity="sha384-" crossorigin="anonymous">
+    
+    <!--[if lte IE 8]>
+        <link rel="stylesheet" href="https://unpkg.com/purecss@0.6.2/build/grids-responsive-old-ie-min.css">
+    <![endif]-->
+    <!--[if gt IE 8]><!-->
+        <link rel="stylesheet" href="https://unpkg.com/purecss@0.6.2/build/grids-responsive-min.css">
+    <!--<![endif]-->
+    
+    
+        <!--[if lte IE 8]>
+            <link rel="stylesheet" href="/st/css/blog-old-ie.css">
+        <![endif]-->
+        <!--[if gt IE 8]><!-->
+            <link rel="stylesheet" href="/st/css/blog.css">
+        <!--<![endif]-->
+</head>
+<body>
+
+
+
+
+
+
+
+<div id="layout" class="pure-g">
+    <div class="sidebar pure-u-1 pure-u-md-1-4">
+        <div class="header">
+		<img src="/st/tee-logo-200.png">
+            <h1 class="brand-title"><a href="/">Trans Europ Express</a></h1>
+            <h2 class="brand-tagline">your virtual travel guide</h2>
+        </div>
+    </div>
+
+    <div class="content pure-u-1 pure-u-md-3-4">
+        <div>
+            <!-- A wrapper for all the blog posts -->
+            <div class="posts">
+                
+                <!-- A single blog post -->
+                <section class="post">
+                    <header class="post-header">
+                        
+                        <h2 class="post-title">Trans Europ Express</h2>
+			
+			<p class="post-meta">
+			    By <a href="http://www.kraftwerk.com/" class="post-author">Kraftwerk</a>, 
+			    original URL <a href="https://www.youtube.com/watch?v=JKHfSwoALYE">https://www.youtube.com/watch?v=JKHfSwoALYE</a>
+                        </p>
+                        
+                    </header>
+
+                    <div class="post-description">
+<br>
+<div class="videoWrapper">
+<iframe width="560" height="315" src=" 
+https://www.youtube.com/embed/JKHfSwoALYE" frameborder="0" 
+allowfullscreen></iframe>
+</div>
+
+<pre>
+Trans-Europe Express
+Trans-Europe Express
+Trans-Europe Express
+Trans-Europe Express
+
+Rendezvous on Champs-Elysees
+Leave Paris in the morning on T.E.E.
+Trans-Europe Express
+Trans-Europe Express
+Trans-Europe Express
+Trans-Europe Express
+
+In Vienna we sit in a late-night cafe
+Straight connection, T.E.E.
+Trans-Europe Express
+Trans-Europe Express
+Trans-Europe Express
+Trans-Europe Express
+
+From station to station
+Back to Dusseldorf City
+Meet Iggy Pop and David Bowie
+Trans-Europe Express
+Trans-Europe Express
+Trans-Europe Express
+Trans-Europe Express
+</pre>
+                    </div>
+                </section>
+            </div>
+
+
+            <div class="footer">
+                <div class="pure-menu pure-menu-horizontal">
+                    <ul>
+                        <li class="pure-menu-item"><a href="http://purecss.io/" class="pure-menu-link">About</a></li>
+                        <li class="pure-menu-item"><a href="http://twitter.com/yuilibrary/" class="pure-menu-link">Twitter</a></li>
+                        <li class="pure-menu-item"><a href="http://github.com/yahoo/pure/" class="pure-menu-link">GitHub</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+</body>
+</html>
