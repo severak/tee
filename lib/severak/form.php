76a75e36782b305e7b765084500fcb5cd8b8930d
Severak <strajt9@seznam.cz>
2017-03-07 00:00:19 +0100

    prechytraceno

--- lib/severak/form.php
+++ lib/severak/form.php
@@ -3,12 +3,15 @@ namespace severak;
 use AdamWathan\Form\FormBuilder;
 use AdamWathan\Form\OldInput\OldInputInterface;
 use AdamWathan\Form\ErrorStore\ErrorStoreInterface;
+
+// severak\form - extended variant of AdamWathan form class
 class form
 extends FormBuilder
 implements OldInputInterface, ErrorStoreInterface
 {
 	public $values = [];
 	public $errors = [];
+	protected $_rules = [];
 	
 	function __construct()
 	{
@@ -23,7 +26,7 @@ implements OldInputInterface, ErrorStoreInterface
 	
 	function getOldInput($name)
 	{
-		return isset($this->values[ $name]) ? $this->values[$name] : '';
+		return isset($this->values[$name]) ? $this->values[$name] : '';
 	}
 	
 	function hasError($key)
@@ -31,11 +34,48 @@ implements OldInputInterface, ErrorStoreInterface
 		return !empty($this->errors[$key]);
 	}
 	
-	// todo - konflikt v rozhraní - jak vyřešit?
-	
-	function getError($key)
+	function getError($key, $format = null)
 	{
 		return empty($this->errors[$key]) ? '' : $this->errors[$key];
+	}
+	
+	// add validation rule
+	public function rule($name, $callback, $message)
+	{
+		$this->_rules[$name][] = ['check'=>$callback, 'message'=>$message];
+	}
+	
+	// validate using rules
+	public function validate()
+	{
+		$valid = true;
+		foreach ($this->_rules as $name => $rules) {
+			foreach ($rules as $rule) {
+				$passed = call_user_func_array($rule['check'], [$this->values[$name], $this->values]);	
+				if (empty($passed)) {
+					$this->errors[$name] = $rule['message'];
+					$valid = false;
+					break;
+				}
+			}
+		}
+		return $valid;
+	}
+	
+	// check if form is valid
+	function isValid()
+	{
+		return empty($this->errors);
+	}
+	
+	// display error message
+	function showError($name)
+	{
+		if ($this->errorStore->hasError($name)) {
+			return '<span class="pure-form-message">' . htmlspecialchars($this->errorStore->getError($name)) . '</span>';
+		}
+		
+		return '';
 	}
 }
 
